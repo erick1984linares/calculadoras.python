6c95ef49f77ca8678d883745574b645021a7c589
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-10-30 14:33:46 -0500

    Add files via upload

---
+++ calculadoras.py
@@ -0,0 +1,227 @@
+#ejercicio 1: calcular el area de un triangulo rectangulo
+area, base, altura=0.0, 0.0, 0.0
+#asignacion de valores
+base=50
+altura=20
+#calculo
+area=(base*altura)/2
+#mostrar valores
+print("base=", base)
+print("altura=", altura)
+print("area=", area)
+#ejercicioc 2: calcular la velocidad
+velocidad, distancia, tiempo=0.0, 0.0, 0.0
+#asigancion de valores
+distancia=250
+tiempo=30
+#calculo
+velocidad=distancia/tiempo
+#mostrar valores
+print("distancia=", distancia)
+print("tiempo=", tiempo)
+print("velocidad=", velocidad)
+#ejercicio 3: calcular el tiempo de encuentro
+tiempo_de_encuentro, velocidad1, velocidad2, distancia=0.0, 0.0, 0.0, 0.0
+#asignacion de valores
+velocidad1=20
+velocidad2=40
+distancia=500
+#calculo
+tiempo_de_encuentro=distancia/(velocidad1 + velocidad2)
+#mostrar valores
+print("velocidad1=", velocidad1)
+print("velocidad2=", velocidad2)
+print("distancia=", distancia)
+print("tiempo_de_encuentro=", tiempo_de_encuentro)
+#ejercicio 4: calcular la altura maxima
+altura_maxima, velocidad_inicial, gravedad=0.0, 0.0, 0.0
+#asignacion de valores
+velocidad_inicial=2
+gravedad=9.8
+#calculo
+altura_maxima=(velocidad_inicial*velocidad_inicial)/gravedad
+#mostrar valores
+print("velocidad_inicial=", velocidad_inicial)
+print("gravedad=", gravedad)
+print("altura_maxima=", altura_maxima)
+#ejercicio 5: calcular el tiempo de vuelo
+tiempo_de_vuelo, velocidad_inicial2, gravedad2=0.0, 0.0, 0.0
+#asignacion de valores
+velocidad_inicial2=10
+gravedad2=10
+#calculo
+tiempo_de_vuelo=(velocidad_inicial2*2)/gravedad2
+#mostrar valores
+print("velocidad_inicial2=", velocidad_inicial2)
+print("gravedad2=", gravedad2)
+print("tiempo_de_vuelo=", tiempo_de_vuelo)
+#ejercicio 6: calcular el area de una esfera
+area_de_esfera, pi, radio=0.0, 0.0, 0.0
+#asignacion de valores
+pi=3.1416
+radio=2
+#calculo
+area_de_esfera=(pi*radio*radio*4)
+#mostrar valores
+print("pi=", pi)
+print("radio=", radio)
+print("area_de_esfera=", area_de_esfera)
+#ejercicio 7: calcular la fuerza
+fuerza, masa, aceleracion=0.0, 0.0, 0.0
+#asignacion de valores
+masa=30
+aceleracion=6
+#calculo
+fuerza=(masa*aceleracion)
+#mostrar valores
+print("masa=", masa)
+print("aceleracion=", aceleracion)
+print("fuerza=", fuerza)
+#ejercicio 8: calcular el trabajo
+trabajo, fuerza, distancia=0.0, 0.0, 0.0
+#asignacion de valores
+fuerza=5060
+distancia=30
+#calculo
+trabajo=(fuerza*distancia)
+#mostrar valores
+print("fuerza=", fuerza)
+print("distancia=", distancia)
+print("trabajo=", trabajo)
+#ejercicio 9: calcular la fuerza de rozamiento
+fuerza_de_rozamiento, u, normal=0.0, 0.0, 0.0
+#asignacion de valores
+u=0.6
+normal=100
+#calculo
+fuerza_de_rozamiento=(u*normal)
+#mostrar valores
+print("u=", u)
+print("normal=", normal)
+print("fuerza de rozamiento=", fuerza_de_rozamiento)
+#ejercicio 10: calcular la energia potencial elastica
+energia_potencial_elastica, k, x=0.0, 0.0, 0.0
+#asigancion de valores
+k=20
+x=1.92
+#calculo
+energia_potencial_elastica=(k*x*x)/2
+#mostrar valores
+print("k=", k)
+print("x=", x)
+print("energia potencial elastica=", energia_potencial_elastica)
+#ejercicio 11: calcular la eficiencia de una maquina
+eficiencia_de_una_maquina, calor_caliente, calor_frio=0.0, 0.0, 0.0
+#asignacion de valores
+calor_caliente=500
+calor_frio=200
+#calculo
+eficiencia_de_una_maquina=(calor_caliente - calor_frio)/calor_caliente
+#mostrar valores
+print("calor_caliente=", calor_caliente)
+print("calor_frio=", calor_frio)
+print("eficiencia_una_maquina=", eficiencia_de_una_maquina)
+#ejercicio 12: calcular el area lateral de un prisma
+area_lateral_de_un_prisma, perimetro_de_la_base, arista=0.0, 0.0, 0.0
+#asignacion de valores
+perimetro_de_la_base=12
+arista=3
+#calculo
+area_lateral_de_un_prisma=(perimetro_de_la_base*arista)
+#mostrar valores
+print("perimetro de la base=", perimetro_de_la_base)
+print("arista=", arista)
+print("area lateral de un prisma=", area_lateral_de_un_prisma)
+#ejercicio 13: calcular el area total de un prisma
+area_total_de_un_prisma, area_de_la_superficie_lateral, area_de_la_base_por_dos=0.0, 0.0, 0.0
+#asigancion de valores
+area_de_la_superficie_lateral=35
+area_de_la_base_por_dos=50
+#calculo
+area_total_de_un_prisma=(area_de_la_superficie_lateral*area_de_la_base_por_dos)
+#mostrar valores
+print("area_de_la_superficie_lateral=", area_de_la_superficie_lateral)
+print("area_de_la_base_por_dos=", area_de_la_base_por_dos)
+print("area_total_de_un_prisma=", area_total_de_un_prisma)
+#ejercicio 14: calcular el volumen de un prisma
+volumen, area_de_la_base, altura2=0.0, 0.0, 0.0
+#asignacion de valores
+area_de_la_base=15
+altura2=5
+#calculo
+volumen=(area_de_la_base*altura2)
+#mostrar valores
+print("area_de_la_base=", area_de_la_base)
+print("altura2=", altura2)
+print("volumen=", volumen)
+#ejercicio 15: calcular el area de un rectangulo
+area_de_un_rectangulo, base1, altura3=0.0, 0.0, 0.0
+#asignacion de valores
+base1=23
+altura3=2
+#calculo
+area_de_un_rectangulo=(base1*altura3)
+#mostar valores
+print("base1=", base1)
+print("altura3=", altura3)
+print("area_de_un_rectangulo=", area_de_un_rectangulo)
+#ejercicio 16: calcular el area de n hexagono regular
+area_de_un_hexagono, apotema, semiperimetro_de_la_base=0.0, 0.0, 0.0
+#asignacion de valores
+apotema=14
+semiperimetro_de_la_base=18
+#calculo
+area_de_un_hexagono=(apotema*semiperimetro_de_la_base)/2
+#mostrar valores
+print("apotema=", apotema)
+print("semiperimetro_de_la_base=", semiperimetro_de_la_base)
+print("area_de_un_hexagono=", area_de_un_hexagono)
+#ejercicio 17: calcular el area lateral de una piramide
+area_de_la_superficie_lateral_de_una_piramide, semiperimetro_de_la_base2, apotema1=0.0, 0.0, 0.0
+#asignacion de valores
+semiperimetro_de_la_base2=45
+apotema1=25
+#calculo
+area_de_la_superficie_lateral_de_una_piramide=(semiperimetro_de_la_base2*apotema1)
+#mostrar valores
+print("semiperimetro de la base 2 =", semiperimetro_de_la_base2)
+print("apotema1=", apotema1)
+print("area de la superficie lateral de una piramide=", area_de_la_superficie_lateral_de_una_piramide)
+#ejercicio 18: calcular la presion hidrostatica
+presion_hidrostatica, densidad_del_liquido, gravedad3, altura4=0.0, 0.0, 0.0, 0.0
+#asignacion de valores
+densidad_del_liquido=0.8
+gravedad3=9.84
+altura4=14
+#calculo
+presion_hidrostatica=(densidad_del_liquido*gravedad3*altura4)
+#mostrar valores
+print("densidad_del_liquido=", densidad_del_liquido)
+print("gravedad3=", gravedad3)
+print("altura4=", altura4)
+print("presion_hidrostatica=", presion_hidrostatica)
+#ejercicio 19: calcular la potencia
+potencia, trabajo1, tiempo1=0.0, 0.0, 0.0
+#asignacion de valores
+trabajo1=3000
+tiempo1=20
+#calculo
+potencia=(trabajo1*tiempo1)
+#mostrar valores
+print("trabajo1=", trabajo1)
+print("tiempo1=", tiempo1)
+print("potencia=", potencia)
+#ejercicio 20: calcular el volumen de un tronco de cilindro
+volumen_del_tronco_de_cilindro, altura5, radio1, radio2=0.0, 0.0, 0.0, 0.0
+#asignacion de valores
+altura5=21
+radio1=3.2
+radio2=5.2
+#calculo
+volumen_del_tronco_de_cilindro=(altura*(radio1*radio1 + radio2*radio2 + radio1*radio2))/3
+#mostrar valores
+print("altura5=", altura5)
+print("radio1=", radio1)
+print("radio2=", radio2)
+print("volumen de un trono de cilindro=", volumen_del_tronco_de_cilindro)
+
